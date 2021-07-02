bdaa0f69d75930afdc1b7cca86449627e557b7e7
ChronoXplorer <57399759+ChronoXplorer@users.noreply.github.com>
2021-07-02 18:05:03 +0700

    Update GameScript.cs
    Adding serializefield to text component

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Text textComponent;
+    
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         
